a7c70bbbdac0a9eb554c42aa98452d1be9e436bc
Rence_Madali <jan.lawrence.madali@gmail.com>
2023-08-16 18:16:31 +0800

    remove unused livewire components, and controller

--- resources/views/livewire/form/peer-rater.blade.php
+++
@@ -1,3 +0,0 @@
-<x-section title='Peer Rater Form'>
-    <h1 class="text-3xl font-semibold capitalize font-noto">This is peer rater form component.</h1>
-</x-section>
--- resources/views/livewire/form/student-rater.blade.php
+++
@@ -1,3 +0,0 @@
-<x-section title='Student Rater Form'>
-    <h1 class="text-3xl font-semibold capitalize font-noto">This is student rater form component.</h1>
-</x-section>
